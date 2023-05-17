0f078c8be26702b4fdf092342c8f0c8a54fb1a97
gfurtadoalmeida <27151609+gfurtadoalmeida@users.noreply.github.com>
2023-05-17 08:28:04 -0300

    Add Azure HTTP client

---
+++ components/esp32_iot_azure/include/esp32_iot_azure/azure_iot_http_client.h
@@ -0,0 +1,115 @@
+#ifndef __ESP32_IOT_AZURE_IOT_HTTP_CLIENT_H__
+#define __ESP32_IOT_AZURE_IOT_HTTP_CLIENT_H__
+
+#include <stdint.h>
+#include "azure_iot_http.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    /**
+     * @typedef azure_http_context_t
+     * @brief Azure HTTP context.
+     */
+    typedef struct azure_http_context_t azure_http_context_t;
+
+    /**
+     * @brief Create an Azure HTTP context.
+     * @note The context must be released by @ref azure_http_free.
+     * @param[in] url The URL to use for this request. Must be null-terminated.
+     * @param[in] url_length The length \p url.
+     * @param[in] path The path to use for this request.
+     * @param[in] path_length The length \p path.
+     * @return @ref azure_http_context_t on success or null on failure.
+     */
+    azure_http_context_t *azure_http_create(const char *url,
+                                            uint32_t url_length,
+                                            const char *path,
+                                            uint32_t path_length);
+
+    /**
+     * @brief Connect the Azure HTTP client.
+     * @param[in] context HTTP context.
+     * @param[in] timeout_ms Connection timeout in milliseconds.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    AzureIoTHTTPResult_t azure_http_connect(azure_http_context_t *context, uint16_t timeout_ms);
+
+    /**
+     * @brief Disconnect the Azure HTTP client.
+     * @param[in] context HTTP context.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    void azure_http_disconnect(azure_http_context_t *context);
+
+    /**
+     * @brief Initialize a size request.
+     * @param[in] context HTTP context.
+     * @param[out] header_buffer The buffer into which the headers for the request will be placed.
+     * @param[in] header_buffer_length The length of \p header_buffer.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    AzureIoTHTTPResult_t azure_http_request_size_init(azure_http_context_t *context,
+                                                      char *header_buffer,
+                                                      uint32_t header_buffer_length);
+
+    /**
+     * @brief Send a size request.
+     * @param[in] context HTTP context.
+     * @param[out] data_buffer The buffer where the response will be placed.
+     * @param[in] data_buffer_length The size of \p data_buffer.
+     * @return The size of the file if success, -1 if failure.
+     */
+    int32_t azure_http_request_size(azure_http_context_t *context,
+                                    char *data_buffer,
+                                    uint32_t data_buffer_length);
+
+    /**
+     * @brief Initialize a get content request.
+     * @param[in] context HTTP context.
+     * @param[out] header_buffer The buffer into which the headers for the request will be placed.
+     * @param[in] header_buffer_length The length of \p header_buffer.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    AzureIoTHTTPResult_t azure_http_init(azure_http_context_t *context,
+                                         char *header_buffer,
+                                         uint32_t header_buffer_length);
+
+    /**
+     * @brief Send a get content request.
+     * @param[in] context HTTP context.
+     * @param[in] range_start The start point for the request payload.
+     * @param[in] range_end The end point for the request payload.
+     * @param[out] data_buffer The buffer into which the response header and payload will be placed.
+     * @param[in] data_buffer_length The length of \p data_buffer.
+     * @param[out] output_data The pointer to the point in the buffer where the payload starts.
+     * @param[out] output_data_length The length of the payload returned by \p output_data.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    AzureIoTHTTPResult_t azure_http_request(azure_http_context_t *context,
+                                            int32_t range_start,
+                                            int32_t range_end,
+                                            char *data_buffer,
+                                            uint32_t data_buffer_length,
+                                            char **output_data,
+                                            uint32_t *output_data_length);
+
+    /**
+     * @brief Deinitialize a get content request.
+     * @param[in] context HTTP context.
+     * @return @ref AzureIoTResult_t with the result of the operation.
+     */
+    AzureIoTHTTPResult_t azure_http_deinit(azure_http_context_t *context);
+
+    /**
+     * @brief Cleanup and free the context.
+     * @param[in] context HTTP context.
+     */
+    void azure_http_free(azure_http_context_t *context);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ components/esp32_iot_azure/src/azure_iot_http_client.c
@@ -0,0 +1,121 @@
+#include <string.h>
+#include "esp32_iot_azure/azure_iot_http_client.h"
+#include "infrastructure/transport.h"
+#include "infrastructure/azure_transport_interface.h"
+
+struct azure_http_context_t
+{
+    AzureIoTTransportInterface_t transport_interface;
+    AzureIoTHTTP_t http;
+    esp_transport_handle_t transport;
+    const char *url;
+    const char *path;
+    uint32_t url_length;
+    uint32_t path_length;
+};
+
+azure_http_context_t *azure_http_create(const char *url,
+                                        uint32_t url_length,
+                                        const char *path,
+                                        uint32_t path_length)
+{
+    azure_http_context_t *context = (azure_http_context_t *)malloc(sizeof(azure_http_context_t));
+
+    memset(context, 0, sizeof(azure_http_context_t));
+
+    context->url = url;
+    context->url_length = url_length;
+    context->path = path;
+    context->path_length = path_length;
+    context->transport = transport_create_tcp();
+
+    azure_transport_interface_init(context->transport, &context->transport_interface);
+
+    return context;
+}
+
+AzureIoTHTTPResult_t azure_http_connect(azure_http_context_t *context, uint16_t timeout_ms)
+{
+    if (transport_connect(context->transport,
+                          context->url,
+                          80,
+                          timeout_ms) == TRANSPORT_STATUS_SUCCESS)
+    {
+        return eAzureIoTHTTPSuccess;
+    }
+
+    return eAzureIoTHTTPError;
+}
+
+void azure_http_disconnect(azure_http_context_t *context)
+{
+    transport_disconnect(context->transport);
+}
+
+AzureIoTHTTPResult_t azure_http_request_size_init(azure_http_context_t *context,
+                                                  char *header_buffer,
+                                                  uint32_t header_buffer_length)
+{
+    return AzureIoTHTTP_RequestSizeInit(&context->http,
+                                        &context->transport_interface,
+                                        context->url,
+                                        context->url_length,
+                                        context->path,
+                                        context->path_length,
+                                        header_buffer,
+                                        header_buffer_length);
+}
+
+int32_t azure_http_request_size(azure_http_context_t *context,
+                                char *data_buffer,
+                                uint32_t data_buffer_length)
+{
+    return AzureIoTHTTP_RequestSize(&context->http,
+                                    data_buffer,
+                                    data_buffer_length);
+}
+
+AzureIoTHTTPResult_t azure_http_init(azure_http_context_t *context,
+                                     char *header_buffer,
+                                     uint32_t header_buffer_length)
+{
+    return AzureIoTHTTP_Init(&context->http,
+                             &context->transport_interface,
+                             context->url,
+                             context->url_length,
+                             context->path,
+                             context->path_length,
+                             header_buffer,
+                             header_buffer_length);
+}
+
+AzureIoTHTTPResult_t azure_http_request(azure_http_context_t *context,
+                                        int32_t range_start,
+                                        int32_t range_end,
+                                        char *data_buffer,
+                                        uint32_t data_buffer_length,
+                                        char **output_data,
+                                        uint32_t *output_data_length)
+{
+    return AzureIoTHTTP_Request(&context->http,
+                                range_start,
+                                range_end,
+                                data_buffer,
+                                data_buffer_length,
+                                output_data,
+                                output_data_length);
+}
+
+AzureIoTHTTPResult_t azure_http_deinit(azure_http_context_t *context)
+{
+    return AzureIoTHTTP_Deinit(&context->http);
+}
+
+void azure_http_free(azure_http_context_t *context)
+{
+    azure_transport_interface_free(&context->transport_interface);
+
+    transport_free(context->transport);
+
+    free(context);
+}
